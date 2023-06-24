e020592838b40333d60cf61c29b4802e76466d30
sebhall <sebhalldesigns@gmail.com>
2023-06-24 13:59:08 +0100

    new file:   universalui_core/src/lib.rs

---
+++ universalui_core/target/debug/deps/universalui_core-5740ce8b4ad1cd33.d
@@ -0,0 +1,5 @@
+C:\Users\sebam\Documents\UniversalUI\universalui_core\target\debug\deps\universalui_core-5740ce8b4ad1cd33.rmeta: src\lib.rs
+
+C:\Users\sebam\Documents\UniversalUI\universalui_core\target\debug\deps\universalui_core-5740ce8b4ad1cd33.d: src\lib.rs
+
+src\lib.rs:
---
+++ universalui_core/target/debug/deps/universalui_core-e8f63d56943ba7c7.d
@@ -0,0 +1,5 @@
+C:\Users\sebam\Documents\UniversalUI\universalui_core\target\debug\deps\universalui_core-e8f63d56943ba7c7.rmeta: src\lib.rs
+
+C:\Users\sebam\Documents\UniversalUI\universalui_core\target\debug\deps\universalui_core-e8f63d56943ba7c7.d: src\lib.rs
+
+src\lib.rs:
