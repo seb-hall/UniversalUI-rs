34e50febf1e685196551336db65201435bb3c96b
seb hall <97197944+seb-hall@users.noreply.github.com>
2023-07-02 13:55:15 +0100

    Delete universalui/target directory

--- universalui/target/debug/deps/instant-9b14a12852b809bf.d
+++
@@ -1,6 +0,0 @@
-C:\Users\sebam\Documents\UniversalUI\universalui\target\debug\deps\instant-9b14a12852b809bf.rmeta: C:\Users\sebam\.cargo\registry\src\index.crates.io-6f17d22bba15001f\instant-0.1.12\src\lib.rs C:\Users\sebam\.cargo\registry\src\index.crates.io-6f17d22bba15001f\instant-0.1.12\src\native.rs
-
-C:\Users\sebam\Documents\UniversalUI\universalui\target\debug\deps\instant-9b14a12852b809bf.d: C:\Users\sebam\.cargo\registry\src\index.crates.io-6f17d22bba15001f\instant-0.1.12\src\lib.rs C:\Users\sebam\.cargo\registry\src\index.crates.io-6f17d22bba15001f\instant-0.1.12\src\native.rs
-
-C:\Users\sebam\.cargo\registry\src\index.crates.io-6f17d22bba15001f\instant-0.1.12\src\lib.rs:
-C:\Users\sebam\.cargo\registry\src\index.crates.io-6f17d22bba15001f\instant-0.1.12\src\native.rs:
--- universalui/target/debug/deps/windows_x86_64_msvc-a80f290f01055436.d
+++
@@ -1,5 +0,0 @@
-C:\Users\sebam\Documents\UniversalUI\universalui\target\debug\deps\windows_x86_64_msvc-a80f290f01055436.rmeta: C:\Users\sebam\.cargo\registry\src\index.crates.io-6f17d22bba15001f\windows_x86_64_msvc-0.42.2\src\lib.rs
-
-C:\Users\sebam\Documents\UniversalUI\universalui\target\debug\deps\windows_x86_64_msvc-a80f290f01055436.d: C:\Users\sebam\.cargo\registry\src\index.crates.io-6f17d22bba15001f\windows_x86_64_msvc-0.42.2\src\lib.rs
-
-C:\Users\sebam\.cargo\registry\src\index.crates.io-6f17d22bba15001f\windows_x86_64_msvc-0.42.2\src\lib.rs:
