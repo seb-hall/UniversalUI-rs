52ed2e56a118ec9c4cc5a24bc9e5396e5ca607b3
seb hall <97197944+seb-hall@users.noreply.github.com>
2023-07-02 13:56:32 +0100

    Delete universalui_windows/target directory

--- universalui_windows/target/debug/build/windows_x86_64_msvc-d29f932bfb40c442/build_script_build-d29f932bfb40c442.d
+++
@@ -1,5 +0,0 @@
-C:\Users\sebam\Documents\UniversalUI\universalui_windows\target\debug\build\windows_x86_64_msvc-d29f932bfb40c442\build_script_build-d29f932bfb40c442.exe: C:\Users\sebam\.cargo\registry\src\index.crates.io-6f17d22bba15001f\windows_x86_64_msvc-0.42.2\build.rs
-
-C:\Users\sebam\Documents\UniversalUI\universalui_windows\target\debug\build\windows_x86_64_msvc-d29f932bfb40c442\build_script_build-d29f932bfb40c442.d: C:\Users\sebam\.cargo\registry\src\index.crates.io-6f17d22bba15001f\windows_x86_64_msvc-0.42.2\build.rs
-
-C:\Users\sebam\.cargo\registry\src\index.crates.io-6f17d22bba15001f\windows_x86_64_msvc-0.42.2\build.rs:
