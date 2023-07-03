56d474cdcfe4fe9dbfa40b8e3fce143a9434c4ee
sebhal <sebhalldesigns@gmail.com>
2023-07-03 11:31:36 +0000

    new file:   universalui_windowing/Cargo.toml 	new file:   universalui_windowing/src/lib.rs

---
+++ universalui_windowing/src/lib.rs
@@ -0,0 +1,6 @@
+//  universalui_windowing   lib.rs
+//  created by sebhall on 03/07/2023
+//
+//  universalui_windowing provides interfaces for the creation
+//  and management of application windows, as well as an interface
+//  
